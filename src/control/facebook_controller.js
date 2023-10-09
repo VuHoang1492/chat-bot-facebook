ee3d86513cc98126b1f611fa3b9d619396281613
VŨ ĐỨC HOÀNG <Hoangbarca1492@gmail.com>
2023-10-09 16:15:38 +0700

    fix facebook service

--- src/control/facebook_controller.js
+++ src/control/facebook_controller.js
@@ -20,7 +20,7 @@ const getWebHookEvent = (req, res) => {
             // Check if the event is a message or postback and
             // pass the event to the appropriate handler function
             if (webhook_event.message) {
-                facebook_service.handleMessage(sender_psid, webhook_event.message);
+                facebookService.handleMessage(sender_psid, webhook_event.message);
             } else if (webhook_event.postback) {
                 facebookService.handlePostback(sender_psid, webhook_event.postback);
             }
