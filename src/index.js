09192419321c9892f8be42b3462dde87c6c3bd29
VŨ ĐỨC HOÀNG <Hoangbarca1492@gmail.com>
2023-10-09 16:11:12 +0700

    change token

--- src/index.js
+++ src/index.js
@@ -17,6 +17,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 
+app.use('/home', (req, res) => {
+    res.send('home')
+})
 
 app.use('/', facebookRoute)
 
